8b6551d1a394a3a6df497684bb8d8ab9451fedb4
Noah Horlacher <kuromarumusic@gmail.com>
2020-06-03 16:35:52 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const {app, BrowserWindow} = require('electron')
+const locals = {/* ...*/}
+const setupPug = require('electron-pug')
+
+app.on('ready', async () => {
+  try {
+    let pug = await setupPug({pretty: true}, locals)
+    pug.on('error', err => console.error('electron-pug error', err))
+  } catch (err) {
+    // Could not initiate 'electron-pug'
+  }
+
+  let mainWindow = new BrowserWindow({ width: 800, height: 600 })
+
+  mainWindow.menuBarVisible = false
+
+  mainWindow.loadURL(`file://${__dirname}/views/index.pug`)
+})
